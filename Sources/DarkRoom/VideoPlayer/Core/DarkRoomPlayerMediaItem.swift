458a0dabed3a9b1c300d0c6c9c911c525a35477d
kiarash vosough <vosough.k@gmail.com>
2022-09-08 23:27:23 +0430

    added license to each file

--- Sources/DarkRoom/VideoPlayer/Core/DarkRoomPlayerMediaItem.swift
+++ Sources/DarkRoom/VideoPlayer/Core/DarkRoomPlayerMediaItem.swift
@@ -4,13 +4,32 @@
 //
 //  Created by Kiarash Vosough on 7/12/22.
 //
+//  Copyright (c) 2022 Divar
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import AVFoundation
 
 // MARK: - Abstraction
 
 /// abstraction for media item that player can load and play.
-/// if using this protocol the item will be used to play and ``DarkRoomPlayer`` won't compose new ``AVPlayerItem``.
+/// if using this protocol the item will be used to play and ``DarkRoomPlayer`` won't compose new `AVPlayerItem`.
 public protocol DarkRoomPlayerMediaItem: DarkRoomPlayerMedia {
 
     /// Item used by the player only once.
